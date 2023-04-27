31f6039f896fdbbb0dbb6d67e68f96c87d390f5a
bbungbbun <tnqkrdbsl@naver.com>
2023-04-27 19:51:36 +0900

    7-2강 수강중

---
+++ class-7-1/class-7-1-ex1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int get_max(int x, int y){
+    if(x > y) return (x);
+    else return (y);
+}
+
+int main(void) {
+	int x, y, max;
+
+    printf("두 정수를 입력하시오");
+
+	return 0;
+}
